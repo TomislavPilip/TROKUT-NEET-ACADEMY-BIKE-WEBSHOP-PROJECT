e186fb71fa220ccb0cd446398a2b5446ae487ffd
TomislavPilip <tpilipac@gmail.com>
2024-05-21 15:04:15 +0200

    update

--- node-exercise-1-TomislavPilip/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express'
-import { registerUser, loginUser } from '../controllers/authController'
-
-const router = express.Router()
-
-router.post('/login', loginUser)
-router.post('/register', registerUser)
-
-export default router
--- node-exercise-1-TomislavPilip/src/routes/homeRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express'
-
-const router = express.Router()
-
-router.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to the homepage!')
-})
-
-export default router
